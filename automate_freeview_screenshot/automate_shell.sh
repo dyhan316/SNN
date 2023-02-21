3606b89cad37dcb18caab8130f729f4081e439ba
dyhan316 <dyhan316@naver.com>
2023-02-21 12:41:54 +0900

    maybe change?

--- automate_freeview_screenshot/automate_shell.sh
+++ automate_freeview_screenshot/automate_shell.sh
@@ -10,10 +10,11 @@ for sub in  `ls $path`
 do 
     sub_path=${path}/${sub}
     echo $sub_path
-    #python3 get_freeview_utils.py -sub_path $sub_path   
-    #cd $sub_path #has to go here direclty for some reason
-    #freeview -cmd cmd_txt.txt
-    #cd $script_path #goes back
+    python3 get_freeview_utils.py -sub_path $sub_path -mode make_cmd
+    cd $sub_path #has to go here direclty for some reason
+    freeview -cmd cmd_txt.txt
+    cd $script_path #goes back
+    python3 get_freeview_utils.py -sub_path $sub_path -mode make_gif
 
     #python get_freeview_utils.py -sub_path /Users/eunmi/Desktop/connectome/sample_data/sub-140989
     #sdkljfsdlkjf #intentially to raise error 
