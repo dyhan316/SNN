8a69d8642a7aa6b5cf83de3298bb0d8ab5b3b579
dyhan316 <dyhan316@naver.com>
2023-02-26 16:50:38 +0900

    now wow

--- automate_freeview_screenshot/.ipynb_checkpoints/make_to_pdf-checkpoint.py
+++ automate_freeview_screenshot/.ipynb_checkpoints/make_to_pdf-checkpoint.py
@@ -3,27 +3,26 @@ import os
 import glob
 import PyPDF2
 
+age_img_path = '/Users/eunmi/Desktop/QSIPREP/make_pdf_test_thing'
+pdf_name = "infantFS_age_0_1_1.pdf"
+
 
 cwd = os.getcwd()
-os.chdir('/Users/eunmi/Desktop/QSIPREP/make_pdf_test_thing')
+os.chdir(age_img_path)
 
 # Get a list of all JPG files in the current directory
 jpg_files = glob.glob('sub-*.jpg')
 
-
 # Create a list of PIL Image objects
 images = [Image.open(jpg_file) for jpg_file in jpg_files]
 
-#font = ImageFont.load("arial.pil")
-#font = ImageFont.load("arial.ttf")
-
 # Create a new PDF document
-pdf_path = os.path.join(cwd,"thing.pdf")
+pdf_path = os.path.join(cwd,pdf_name)
 pdf_image_list = []
 for i, image in enumerate(images):
     # Add some text to the image
     draw = ImageDraw.Draw(image)
-    text = f"sub-111585"
+    text = jpg_files[i]
     font = ImageFont.truetype(os.path.join(cwd,"arial.ttf"), 100)  # Set font size to 36
     draw.text((50, 50), text, font = font)
     
